c1c293c931628fe64edb91d50af1bf1ed48e14b5
aml <aml@aml-PC.domainetap.local>
2016-11-05 08:00:00 +0100

    test trigger jenkins

--- src/main/java/com/ecommerce/domain/User.java
+++ src/main/java/com/ecommerce/domain/User.java
@@ -1,5 +1,16 @@
 package com.ecommerce.domain;
 
 public class User {
-	//daed3a5f173a1b3b624bfb84146ff53ac64fd2e8
+	// daed3a5f173a1b3b624bfb84146ff53ac64fd2e8
+
+	private int id;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
 }
